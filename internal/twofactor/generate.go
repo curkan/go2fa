2d70a504cfa4ecb0d942ec57855d330440e4a58a
Nikita Tsurkan <necit36@gmail.com>
2024-06-25 15:30:07 +0300

    SCR #0 - add copy code and add secret to model

--- internal/twofactor/generate.go
+++ internal/twofactor/generate.go
@@ -2,29 +2,9 @@ package twofactor
 
 import (
 	"encoding/base32"
-	"time"
 
-	otp "github.com/pquerna/otp"
-	"github.com/pquerna/otp/totp"
 	"github.com/xlzd/gotp"
 )
-
-func GeneratePassCode(utf8string string) string {
-	secret := base32.StdEncoding.EncodeToString([]byte(utf8string))
-
-	passcode, err := totp.GenerateCodeCustom(secret, time.Now(), totp.ValidateOpts{
-		Period:    30,
-		Skew:      1,
-		Digits:    otp.DigitsSix,
-		Algorithm: otp.AlgorithmSHA512,
-	})
-
-	if err != nil {
-			panic(err)
-	}
-
-	return passcode
-}
 
 
 func GenerateTOTP(utf8string string) (string, int64) {
