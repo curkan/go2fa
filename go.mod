9b54ce8f7732cee23417b9c60b7082648b98bf07
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-06-25 13:37:53 +0300

    SCR #0 - add code to line

--- go.mod
+++ go.mod
@@ -29,11 +29,13 @@ require (
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/pquerna/otp v1.4.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
--- internal/screens/listKeysScreen.go
+++ internal/screens/listKeysScreen.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/muesli/termenv"
+	"golang.org/x/term"
 )
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
@@ -21,21 +21,43 @@ func (d ItemDelegate) Height() int                             { return 1 }
 func (d ItemDelegate) Spacing() int                            { return 0 }
 func (d ItemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
 func (d ItemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
+	var (
+		title, desc, code  string
+		s = list.NewDefaultItemStyles()
+	)
+
+	s.SelectedTitle = s.SelectedTitle.Foreground(lipgloss.Color("#99dd99")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
+	s.SelectedDesc = s.SelectedDesc.Foreground(lipgloss.Color("#7aa37a")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
+
+	if i, ok := listItem.(list.DefaultItem); ok {
+		title = i.Title()
+		desc = i.Description()
+		code = "123 123"
+	} else {
 		return
 	}
 
-	str := fmt.Sprintf("%d. %s", index+1, i.title)
+	// Conditions
+	var (
+		isSelected  = index == m.Index()
+	)
 
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
+	width, _, _ := term.GetSize(0)
+	width = 50
+	padding := 0
+	if isSelected && m.FilterState() != list.Filtering {
+		padding = width - len(title) - len(code) - 5
+		title = s.SelectedTitle.Render(title)
+		desc = s.SelectedDesc.Render(desc)
+	} else {
+		padding = width - len(title) - len(code) - 5
+		title = s.NormalTitle.Render(title)
+		desc = s.NormalDesc.Render(desc)
 	}
 
-	fmt.Fprint(w, fn(str))
+
+	fmt.Fprintf(w, "%s%*s%s\n%s", title, padding, " ", code, desc)
+	// fmt.Printf("%s%*s%s\n%s\n", title, padding, "", code, desc)
 }
 
 type itemKey struct {
@@ -93,11 +115,11 @@ func ListKeysScreen() listKeysModel {
 		itemKey{title: "Gitlab vcsx", desc: "http://vcsxa.egamings.com"},
 	}
 
-	delegate := list.NewDefaultDelegate()
-	delegate.Styles.SelectedTitle = delegate.Styles.SelectedTitle.Foreground(lipgloss.Color("#99dd99")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
-	delegate.Styles.SelectedDesc = delegate.Styles.SelectedDesc.Foreground(lipgloss.Color("#7aa37a")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
+	// delegate := list.NewDefaultDelegate()
+	// delegate.Styles.SelectedTitle = delegate.Styles.SelectedTitle.Foreground(lipgloss.Color("#99dd99")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
+	// delegate.Styles.SelectedDesc = delegate.Styles.SelectedDesc.Foreground(lipgloss.Color("#7aa37a")).BorderLeftForeground(lipgloss.Color("#7aa37a"))
 
-	m := listKeysModel{list: list.New(itemKeys, delegate, 30, 20)}
+	m := listKeysModel{list: list.New(itemKeys, ItemDelegate{}, 30, 20)}
 	// m := listKeysModel{list: list.New(itemKeys, list.NewDefaultDelegate(), 30, 20)}
 	m.list.Title = "Доступные ключи"
 
