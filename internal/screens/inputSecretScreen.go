282859086fcde3a7a5476770e81d065010c39ed9
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-06-25 08:50:23 +0300

    SCR #0 - init go2fa

---
+++ internal/screens/inputSecretScreen.go
@@ -0,0 +1,87 @@
+package screens
+
+// A simple example demonstrating the use of multiple text input components
+// from the Bubbles component library.
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/muesli/termenv"
+)
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+	helpStyleInput      = blurredStyle
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+type (
+	errMsg error
+)
+
+type screenInputSecret struct {
+	textInput     textinput.Model
+	err       error
+}
+
+func ScreenInputSecret() screenInputSecret {
+	ti := textinput.New()
+	ti.Placeholder = "SecretKey"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return screenInputSecret{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m screenInputSecret) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m screenInputSecret) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+			case tea.KeyEsc:
+				screen := ScreenOne()
+				return RootScreen().SwitchScreen(&screen)
+
+			case tea.KeyCtrlC:
+				output := termenv.NewOutput(os.Stdout)
+				output.ClearScreen()
+				return m, tea.Quit
+
+			case tea.KeyEnter:
+				secretKey := m.textInput.Value()
+				fmt.Print(secretKey)
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m screenInputSecret) View() string {
+	return quitTextStyle.Render(
+		fmt.Sprintf("Введите Secret Key\n\n%s\n\n%s", m.textInput.View(), "(esc для возврата назад)",) + "\n",
+	)
+}
---
+++ internal/screens/twoScreen.go
@@ -0,0 +1,99 @@
+package screens
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	// Available spinners
+	spinners = []spinner.Spinner{
+		spinner.Line,
+		spinner.Dot,
+		spinner.MiniDot,
+		spinner.Jump,
+		spinner.Pulse,
+		spinner.Points,
+		spinner.Globe,
+		spinner.Moon,
+		spinner.Monkey,
+	}
+
+	textStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("252")).Render
+	spinnerStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("69"))
+	helpStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
+)
+
+type screenTwoModel struct {
+	index   int
+    spinner spinner.Model
+    err     error
+}
+
+func ScreenTwo() screenTwoModel {
+    s := spinner.New()
+	s.Spinner = spinner.Dot
+    return screenTwoModel{
+        spinner: s,
+    }
+}
+
+func (m screenTwoModel) Init() tea.Cmd {
+    return m.spinner.Tick
+}
+
+func (m screenTwoModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+		case "h", "left":
+			m.index--
+			if m.index < 0 {
+				m.index = len(spinners) - 1
+			}
+			m.resetSpinner()
+			return m, m.spinner.Tick
+		case "l", "right":
+			m.index++
+			if m.index >= len(spinners) {
+				m.index = 0
+			}
+
+			screen_y := ScreenOne()
+            return RootScreen().SwitchScreen(&screen_y)
+		default:
+			return m, nil
+		}
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	default:
+		return m, nil
+	}
+}
+
+func (m *screenTwoModel) resetSpinner() {
+	m.spinner = spinner.New()
+	m.spinner.Style = spinnerStyle
+	m.spinner.Spinner = spinners[m.index]
+}
+
+func (m screenTwoModel) View() (s string) {
+	var gap string
+	switch m.index {
+	case 1:
+		gap = ""
+	default:
+		gap = " "
+	}
+
+	s += fmt.Sprintf("\n %s%s%s\n\n", m.spinner.View(), gap, textStyle("Spinning..."))
+	s += helpStyle("h/l, ←/→: change spinner • q: exit\n")
+	return
+}
