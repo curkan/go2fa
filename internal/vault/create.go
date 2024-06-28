b620214bf144087840a0920be1324317cd23f72f
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2024-06-28 19:06:48 +0300

    SCR #0 - fix create default dirs

--- internal/vault/create.go
+++ internal/vault/create.go
@@ -3,6 +3,7 @@ package vault
 import "go2fa/internal/crypto"
 
 func Create() bool {
+	crypto.CreateDirs()
 	vault := crypto.GetEmptyVault()
 	crypto.GeneratePublicPrivateKeys()
 	crypto.SetEmptyVault(vault)
